75813252fc8175b4759480c6ef701e35f8d747e7
Frankenfeld Kira <kf064@hdm-stuttgart.de>
2021-07-27 15:30:15 +0200

    fixed space between title and description

--- frontend/src/components/Home/GameCategoriesList/GameCategory/Game/Game.js
+++ frontend/src/components/Home/GameCategoriesList/GameCategory/Game/Game.js
@@ -66,7 +66,7 @@ function Game(props) {
 
     return (
         <div className='m-5'>
-            <div className="d-flex align-items-center justify-content-center">
+            <div className="d-flex align-items-center justify-content-center" id="collapse-top">
                 <button id={ "desciption-collapse-btn-" + props.gameId } className='home-collapse-button' type="button" data-toggle="collapse" data-target={ "#desciption-collapse-" + props.gameId } aria-expanded="false" aria-controls="collapseExample" onClick={ setCollapse }>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
